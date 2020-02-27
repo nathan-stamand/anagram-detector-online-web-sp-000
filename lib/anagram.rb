3d6fe25b16e13f30395b3b26f6bf6f99a07e66a7
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-27 13:35:28 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,10 +2,17 @@ class Anagram
   attr_accessor :word
   
   def initialize(word)
-    @word = word.split.sort
+    @word = word
   end
   
   def match(list)
-    
+    anagrams = []
+    list.each do |possible_anagram|
+      if possible_anagram.split("").sort == @word.split("").sort
+        anagrams << possible_anagram
+      end
+    end
+    anagrams    
+  end
   
 end 
